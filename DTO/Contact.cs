c227eb7741dbc26d335b1210310cd508a82854d1
vanhung1999dev <vanhung1999dev@gmail.com>
2020-12-16 22:07:53 +0700

    add Contact class

---
+++ DTO/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Contact
+    {
+        public string contact_ID { get; set; }
+        public string username { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string note { get; set; }
+        public DateTime create_date {get;set;}
+        public string status_Contact { get; set; }
+        public string product_Name { get; set; }
+
+        public Contact()
+        {
+
+        }
+    }
+
+}
