b45fe07e49d1936ff1ea989504c51fefec6e5184
LuongLegend <luongprolv99@gmail.com>
2020-12-23 20:16:28 +0700

    done

--- GUI/InsertProduct.aspx.cs
+++ GUI/InsertProduct.aspx.cs
@@ -21,6 +21,7 @@ namespace GUI
             {
                 Response.Redirect("/Home.aspx");
             }
+            //list catagories
             DataTable allCat = pro.getAllCat();
             int count = 0;
             foreach (DataRow r in allCat.Rows)
@@ -31,6 +32,29 @@ namespace GUI
                 CheckBoxList2.Items.Add(it);                
                 count++;
             }
+            //list bonus
+            count = 0;
+            DataTable allBonus = pro.getAllBonus();
+            foreach (DataRow r in allBonus.Rows)
+            {
+                string bonusId = r["bonus_ID"].ToString();
+                string bonusName = r["name_bonus"].ToString();
+                ListItem it = new ListItem(bonusName, bonusId);
+                bonusCheckBox.Items.Add(it);
+                count++;
+            }
+            //list sale
+            count = 0;
+            DataTable allSale = pro.getAllSale();
+            foreach (DataRow r in allSale.Rows)
+            {
+                string saleId = r["sale_ID"].ToString();
+                string saleName = "Giảm " + r["sale_price"].ToString() + " khi mua " + r["min_product"].ToString() + " sản phẩm";
+                ListItem it = new ListItem(saleName, saleId);
+                dropDownListSale.Items.Add(it);
+                count++;
+            }
+
         }
 
         protected void validateProductID(object sender, ServerValidateEventArgs e)
@@ -73,6 +97,7 @@ namespace GUI
               
                 pro.insertProduct(p.Product_id_, p.Product_name_, p.Product_brand_, p.Origin_, p.Summary_, p.Price_, p.Quantity_, p.Photo_, z2);
                 saveUpLoadFile();
+                //insert into product_catagory
                 foreach (ListItem item in CheckBoxList2.Items)
                 {
                     if (item.Selected)
@@ -80,6 +105,16 @@ namespace GUI
                         pro.insertProductCatagory(p.Product_id_, item.Value);
                     }
                 }
+                //insert into product_bonus
+                foreach (ListItem item in bonusCheckBox.Items)
+                {
+                    if (item.Selected)
+                    {
+                        pro.insertProductBonus(p.Product_id_, item.Value);
+                    }
+                }
+                //insert into product_sale
+                pro.insertProductSale(p.Product_id_, dropDownListSale.SelectedValue);
                 Response.Redirect("Products.aspx");
             }
         }
